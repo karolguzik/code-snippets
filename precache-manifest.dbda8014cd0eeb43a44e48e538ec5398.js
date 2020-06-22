90c01a9b53e4b83e570db217feff16ae0642dc8a
Karol <guzik912@o2.pl>
2020-06-22 09:28:22 +0200

    Updates

---
+++ precache-manifest.dbda8014cd0eeb43a44e48e538ec5398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab4f5f02e1fa8962cafd1693b615c69",
+    "url": "/code-snippets/index.html"
+  },
+  {
+    "revision": "7ad93758a45c1717f430",
+    "url": "/code-snippets/static/css/main.ed2fb82b.chunk.css"
+  },
+  {
+    "revision": "482e24f72bc7bb62df53",
+    "url": "/code-snippets/static/js/2.a700419f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/code-snippets/static/js/2.a700419f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad93758a45c1717f430",
+    "url": "/code-snippets/static/js/main.6dfbd951.chunk.js"
+  },
+  {
+    "revision": "62f5aab5e21ac2ad0b84",
+    "url": "/code-snippets/static/js/runtime-main.ab292aa1.js"
+  },
+  {
+    "revision": "4151cde581d413aff6f3adc8b2f1e18b",
+    "url": "/code-snippets/static/media/bgc.4151cde5.png"
+  }
+]);
